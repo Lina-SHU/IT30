861b771acdf2d2f8ba520aec02f6b3376d0a9d47
Lina-SHU <sakura0477@gmail.com>
2024-08-08 16:07:16 +0800

    fix: accordion value

--- src/views/AccordionView.vue
+++ src/views/AccordionView.vue
@@ -6,6 +6,7 @@ const route = useRoute();
 const menuList = ref([
     {
         title: 'Header I',
+        value: '0',
         items: [
             {
                 name: 'page1',
@@ -15,6 +16,7 @@ const menuList = ref([
     },
     {
         title: 'Header II',
+        value: '1',
         items: [
             {
                 name: 'page2',
@@ -24,24 +26,24 @@ const menuList = ref([
     }
 ])
 
-const panelValue = ref('1');
-const checkActiveRoute = (menuroute, index) => {
+const panelValue = ref('0');
+const checkActiveRoute = (menuroute, value) => {
     if (route.fullPath === menuroute.path) {
-        panelValue.value = index.toString(); // 開啟該主選單
+        panelValue.value = value; // 開啟該主選單
     }
     return route.fullPath === menuroute.path;
 }
 </script>
     
 <template>
-    <main class="p-6">
+    <main class="bg-surface-200 min-h-screen p-6">
         <div class="grid grid-cols-6 gap-4">
             <div class="col-span-2">
                 <Accordion :value="panelValue">
-                    <AccordionPanel :value="index.toString()" v-for="(menu, index) in menuList" :key="menu">
+                    <AccordionPanel :value="menu.value" v-for="menu in menuList" :key="menu">
                         <AccordionHeader>{{ menu.title }}</AccordionHeader>
                         <AccordionContent>
-                            <router-link v-for="item in menu.items" :key="item" :to="item.path" :class="{ 'text-primary-300': checkActiveRoute(item, index) }">{{ item.name }}</router-link>
+                            <router-link v-for="item in menu.items" :key="item" :to="item.path" :class="{ 'text-primary-500': checkActiveRoute(item, menu.value) }">{{ item.name }}</router-link>
                         </AccordionContent>
                     </AccordionPanel>
                 </Accordion>
